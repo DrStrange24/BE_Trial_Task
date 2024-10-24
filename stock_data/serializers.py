b8e9b18b3d31040eeedcbae417f96f7db268fe91
anonymous <jbrynnbacuta@gmail.com>
2024-10-24 10:48:36 +0800

    reset all

--- stock_data/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class BacktestSerializer(serializers.Serializer):
-    stock_symbol = serializers.CharField(max_length=10)
-    initial_investment = serializers.FloatField()
-    short_window = serializers.IntegerField(default=50)
-    long_window = serializers.IntegerField(default=200)
--- stock_data/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BacktestView
-
-urlpatterns = [
-    path('backtest/', BacktestView.as_view(), name='backtest'),
-]
