7b613d2c11127a6652c70362389184fc051d8567
anonymous <jbrynnbacuta@gmail.com>
2024-10-24 16:58:05 +0800

    fetch_data on view

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import StockData
 
-# Register your models here.
+@admin.register(StockData)
+class StockDataAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'date', 'open_price', 'close_price', 'high_price', 'low_price', 'volume')
+    search_fields = ('symbol',)
